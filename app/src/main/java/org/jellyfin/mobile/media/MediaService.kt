8f760061195b52dfb4246476370ad1933aabf9ea
Maxr1998 <max.rumpf1998@gmail.com>
2020-11-01 00:32:25 +0100

    Improve MediaService (WIP)
    Uses code from the android/uamp reference media player project

--- app/src/main/java/org/jellyfin/mobile/media/MediaService.kt
+++ app/src/main/java/org/jellyfin/mobile/media/MediaService.kt
@@ -1,5 +1,10 @@
+// Contains code adapted from https://github.com/android/uamp/blob/main/common/src/main/java/com/example/android/uamp/media/MediaService.kt
+
 package org.jellyfin.mobile.media
 
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.ResultReceiver
@@ -9,12 +14,22 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
+import android.widget.Toast
+import androidx.annotation.WorkerThread
+import androidx.core.content.ContextCompat
 import androidx.media.MediaBrowserServiceCompat
-import com.google.android.exoplayer2.ControlDispatcher
-import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.SimpleExoPlayer
+import androidx.mediarouter.media.MediaControlIntent
+import androidx.mediarouter.media.MediaRouteSelector
+import androidx.mediarouter.media.MediaRouter
+import androidx.mediarouter.media.MediaRouterParams
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.audio.AudioAttributes
+import com.google.android.exoplayer2.ext.cast.CastPlayer
+import com.google.android.exoplayer2.ext.cast.SessionAvailabilityListener
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.ext.mediasession.TimelineQueueNavigator
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.gms.cast.framework.CastContext
 import kotlinx.coroutines.*
 import org.jellyfin.apiclient.interaction.ApiClient
 import org.jellyfin.apiclient.model.dto.BaseItemDto
@@ -30,6 +45,7 @@ import org.jellyfin.mobile.controller.ServerController
 import org.jellyfin.mobile.model.sql.entity.ServerUser
 import org.jellyfin.mobile.utils.*
 import org.koin.android.ext.android.inject
+import timber.log.Timber
 import java.net.URLEncoder
 import java.util.*
 import com.google.android.exoplayer2.MediaItem as ExoPlayerMediaItem
@@ -41,17 +57,45 @@ class MediaService : MediaBrowserServiceCompat() {
 
     private val serviceJob = SupervisorJob()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+    private var isForegroundService = false
 
     private lateinit var loadingJob: Job
     private var serverUser: ServerUser? = null
+
+    // The current player will either be an ExoPlayer (for local playback) or a CastPlayer (for
+    // remote playback through a Cast device).
+    private lateinit var currentPlayer: Player
+
+    private lateinit var notificationManager: MediaNotificationManager
     private lateinit var mediaController: MediaControllerCompat
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
+    private lateinit var mediaRouteSelector: MediaRouteSelector
+    private lateinit var mediaRouter: MediaRouter
+    private val mediaRouterCallback = MediaRouterCallback()
 
     private var currentPlaylistItems: MutableList<MediaDescriptionCompat> = mutableListOf()
 
+    private val playerAudioAttributes = AudioAttributes.Builder()
+        .setContentType(C.CONTENT_TYPE_MUSIC)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
+
+    private val playerListener = PlayerEventListener()
+
     private val exoPlayer: SimpleExoPlayer by lazy {
-        SimpleExoPlayer.Builder(this).build()
+        SimpleExoPlayer.Builder(this).build().apply {
+            setAudioAttributes(playerAudioAttributes, true)
+            setHandleAudioBecomingNoisy(true)
+            addListener(playerListener)
+        }
+    }
+
+    private val castPlayer: CastPlayer by lazy {
+        CastPlayer(CastContext.getSharedInstance(this)).apply {
+            setSessionAvailabilityListener(CastSessionAvailabilityListener())
+            addListener(playerListener)
+        }
     }
 
     /**
@@ -84,11 +128,22 @@ class MediaService : MediaBrowserServiceCompat() {
             }
         }
 
+        val sessionActivityPendingIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let { sessionIntent ->
+            PendingIntent.getActivity(this, 0, sessionIntent, 0)
+        }
+
         mediaSession = MediaSessionCompat(this, "MediaService").apply {
+            setSessionActivity(sessionActivityPendingIntent)
             isActive = true
         }
 
         sessionToken = mediaSession.sessionToken
+
+        notificationManager = MediaNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            PlayerNotificationListener()
+        )
 
         mediaController = MediaControllerCompat(this, mediaSession)
 
@@ -97,6 +152,21 @@ class MediaService : MediaBrowserServiceCompat() {
             setPlaybackPreparer(MediaPlaybackPreparer())
             setQueueNavigator(MediaQueueNavigator(mediaSession))
         }
+
+        mediaRouter = MediaRouter.getInstance(this)
+        mediaRouter.setMediaSessionCompat(mediaSession)
+        mediaRouteSelector = MediaRouteSelector.Builder().apply {
+            addControlCategory(MediaControlIntent.CATEGORY_REMOTE_PLAYBACK)
+        }.build()
+        mediaRouter.routerParams = MediaRouterParams.Builder().build()
+        mediaRouter.addCallback(mediaRouteSelector, mediaRouterCallback, MediaRouter.CALLBACK_FLAG_REQUEST_DISCOVERY)
+
+        switchToPlayer(
+            previousPlayer = null,
+            newPlayer = if (castPlayer.isCastSessionAvailable) castPlayer else exoPlayer
+        )
+        notificationManager.showNotificationForPlayer(currentPlayer)
+
     }
 
     override fun onDestroy() {
@@ -108,7 +178,12 @@ class MediaService : MediaBrowserServiceCompat() {
         // Cancel coroutines when the service is going away
         serviceJob.cancel()
 
+        // Free ExoPlayer resources
+        exoPlayer.removeListener(playerListener)
         exoPlayer.release()
+
+        // Stop listening for route changes.
+        mediaRouter.removeCallback(mediaRouterCallback)
     }
 
     override fun onGetRoot(
@@ -142,6 +217,7 @@ class MediaService : MediaBrowserServiceCompat() {
         }
     }
 
+    @WorkerThread
     private suspend fun loadView(parentId: String, result: Result<List<MediaItem>>) {
         when (parentId) {
             /**
@@ -481,6 +557,73 @@ class MediaService : MediaBrowserServiceCompat() {
         return MediaItem(description, flag)
     }
 
+    /**
+     * Load the supplied list of songs and the song to play into the current player.
+     */
+    /*private fun preparePlaylist(
+        metadataList: List<MediaMetadataCompat>,
+        itemToPlay: MediaMetadataCompat?,
+        playWhenReady: Boolean,
+        playbackStartPositionMs: Long
+    ) {
+        // Since the playlist was probably based on some ordering (such as tracks
+        // on an album), find which window index to play first so that the song the
+        // user actually wants to hear plays first.
+        val initialWindowIndex = if (itemToPlay == null) 0 else metadataList.indexOf(itemToPlay)
+        currentPlaylistItems = metadataList
+
+        currentPlayer.playWhenReady = playWhenReady
+        currentPlayer.stop(true)
+        if (currentPlayer == exoPlayer) {
+            val mediaSource = metadataList.toMediaSource(dataSourceFactory)
+            exoPlayer.prepare(mediaSource)
+            exoPlayer.seekTo(initialWindowIndex, playbackStartPositionMs)
+        } else /* currentPlayer == castPlayer */ {
+            val items: Array<MediaQueueItem> = metadataList.map {
+                it.toMediaQueueItem()
+            }
+            castPlayer.setMediaItems(
+                items,
+                initialWindowIndex,
+                playbackStartPositionMs,
+            )
+        }
+    }*/
+
+    private fun switchToPlayer(previousPlayer: Player?, newPlayer: Player) {
+        if (previousPlayer == newPlayer) {
+            return
+        }
+        currentPlayer = newPlayer
+        if (previousPlayer != null) {
+            val playbackState = previousPlayer.playbackState
+            if (currentPlaylistItems.isEmpty()) {
+                // We are joining a playback session.
+                // Loading the session from the new player is not supported, so we stop playback.
+                currentPlayer.stop(true)
+            } else if (playbackState != Player.STATE_IDLE && playbackState != Player.STATE_ENDED) {
+                /*preparePlaylist(
+                    metadataList = currentPlaylistItems,
+                    itemToPlay = currentPlaylistItems[previousPlayer.currentWindowIndex],
+                    playWhenReady = previousPlayer.playWhenReady,
+                    playbackStartPositionMs = previousPlayer.currentPosition
+                )*/
+            }
+        }
+        mediaSessionConnector.setPlayer(newPlayer)
+        previousPlayer?.stop(true)
+    }
+
+    private inner class CastSessionAvailabilityListener : SessionAvailabilityListener {
+        override fun onCastSessionAvailable() {
+            switchToPlayer(currentPlayer, castPlayer)
+        }
+
+        override fun onCastSessionUnavailable() {
+            switchToPlayer(currentPlayer, exoPlayer)
+        }
+    }
+
     private inner class MediaQueueNavigator(mediaSession: MediaSessionCompat) :
         TimelineQueueNavigator(mediaSession) {
         override fun getMediaDescription(
@@ -550,6 +693,86 @@ class MediaService : MediaBrowserServiceCompat() {
                 "EnableRemoteMedia=true"
 
             return ExoPlayerMediaItem.fromUri(url)
+        }
+    }
+
+    /**
+     * Listen for notification events.
+     */
+    private inner class PlayerNotificationListener : PlayerNotificationManager.NotificationListener {
+        override fun onNotificationPosted(notificationId: Int, notification: Notification, ongoing: Boolean) {
+            if (ongoing && !isForegroundService) {
+                val serviceIntent = Intent(applicationContext, this@MediaService.javaClass)
+                ContextCompat.startForegroundService(applicationContext, serviceIntent)
+
+                startForeground(notificationId, notification)
+                isForegroundService = true
+            }
+        }
+
+        override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
+            stopForeground(true)
+            isForegroundService = false
+            stopSelf()
+        }
+    }
+
+    /**
+     * Listen for events from ExoPlayer.
+     */
+    private inner class PlayerEventListener : Player.EventListener {
+        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+            when (playbackState) {
+                Player.STATE_BUFFERING,
+                Player.STATE_READY -> {
+                    notificationManager.showNotificationForPlayer(currentPlayer)
+                    if (playbackState == Player.STATE_READY) {
+                        // When playing/paused save the current media item in persistent
+                        // storage so that playback can be resumed between device reboots.
+                        // Search for "media resumption" for more information.
+                        // TODO saveRecentSongToStorage()
+
+                        if (!playWhenReady) {
+                            // If playback is paused we remove the foreground state which allows the
+                            // notification to be dismissed. An alternative would be to provide a
+                            // "close" button in the notification which stops playback and clears
+                            // the notification.
+                            stopForeground(false)
+                        }
+                    }
+                }
+                else -> notificationManager.hideNotification()
+            }
+        }
+
+        override fun onPlayerError(error: ExoPlaybackException) {
+            var message = R.string.media_service_generic_error
+            when (error.type) {
+                ExoPlaybackException.TYPE_SOURCE -> {
+                    message = R.string.media_service_item_not_found
+                    Timber.e("TYPE_SOURCE: %s", error.sourceException.message)
+                }
+                ExoPlaybackException.TYPE_RENDERER -> Timber.e("TYPE_RENDERER: %s", error.rendererException.message)
+                ExoPlaybackException.TYPE_UNEXPECTED -> Timber.e("TYPE_UNEXPECTED: %s", error.unexpectedException.message)
+                ExoPlaybackException.TYPE_REMOTE -> Timber.e("TYPE_REMOTE: %s", error.message)
+                ExoPlaybackException.TYPE_OUT_OF_MEMORY -> Timber.e("TYPE_OUT_OF_MEMORY: %s", error.outOfMemoryError.message)
+                ExoPlaybackException.TYPE_TIMEOUT -> Timber.e("TYPE_TIMEOUT: %s", error.timeoutException.message)
+            }
+            applicationContext.toast(message, Toast.LENGTH_LONG)
+        }
+    }
+
+    /**
+     * Listen for MediaRoute changes
+     */
+    private inner class MediaRouterCallback : MediaRouter.Callback() {
+        override fun onRouteSelected(router: MediaRouter, route: MediaRouter.RouteInfo, reason: Int) {
+            if (reason == MediaRouter.UNSELECT_REASON_ROUTE_CHANGED) {
+                Timber.d("Unselected because route changed, continue playback")
+            } else if (reason == MediaRouter.UNSELECT_REASON_STOPPED) {
+                Timber.d("Unselected because route was stopped, stop playback")
+                currentPlayer.stop()
+            }
         }
     }
 
